b15bda2f1792be906829a56c620bf9198ed090ba
MRozmus <marcin.rozmus97@gmail.com>
2020-02-07 00:41:03 +0100

    Add 'devise' gem & Admin model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admins, skip: :registrations
+
   root "products#index"
-
   resources :products
-
   get 'products,:sort_params', to: 'products#index', as: :sorted_products
 end
